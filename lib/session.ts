6e59fe4a16b76444165fe0d50d3c9ef9631b5cc3
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-12-18 21:37:26 -0500

    auth

---
+++ lib/session.ts
@@ -0,0 +1,54 @@
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from 'jsonwebtoken'
+import { JWT } from "next-auth/jwt";
+
+import { getUser, createUser } from "@/lib/actions";
+import { SessionInterface, UserProfile } from "@/common.types";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+//   jwt: {
+//     encode: ({ secret, token }) => {
+//       const encodedToken = jsonwebtoken.sign(
+//         {
+//           ...token,
+//           iss: "grafbase",
+//           exp: Math.floor(Date.now() / 1000) + 60 * 60,
+//         },
+//         secret
+//       );
+      
+//       return encodedToken;
+//     },
+//     decode: async ({ secret, token }) => {
+//       const decodedToken = jsonwebtoken.verify(token!, secret);
+//       return decodedToken as JWT;
+//     },
+//   },
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.svg",
+  },
+  callbacks: {
+    async session({ session }) {
+        return session;
+    },
+    async signIn({ user}: { user: AdapterUser | User }) {
+        try {
+            return true
+        }
+        catch (error: any) {
+            console.error(error)
+           return false
+        }
+    }
+  }
+}
